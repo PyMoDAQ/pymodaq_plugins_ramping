d8065e1da81c599d99aae8181a35edef7fa19413
Sébastien Weber <seba.weber@gmail.com>
2026-08-04 23:02:23 +0200

    treating multidimensional data
    Nans had to be dealt with in pymodaq find_index
    maybe still one issue with clipping data to the len of the indexes array, the opposite may be needed given the smaller array
    res = np.sum((one_hot_t * data[0:len(indexes)]), axis=1) / counts

--- src/pymodaq_plugins_ramping/utilities/histograming.py
+++ src/pymodaq_plugins_ramping/utilities/histograming.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from qtpy import QtWidgets, QtCore
 from pymodaq_utils.logger import set_logger, get_module_name
 from pymodaq.utils.shared_ui import SharedUI
-from pymodaq_data import DataToExport, DataWithAxes, DataCalculated, Axis
+from pymodaq_data import DataToExport, DataWithAxes, DataCalculated, Axis, DataDistribution
 from pymodaq_data.h5modules.data_saving import DataLoader, Node
 from pymodaq_gui.h5modules.saving import H5Saver
 from pymodaq_gui.managers.parameter_manager import ParameterManager, Parameter
@@ -40,7 +40,7 @@ class HistogramPlot(CustomApp):
         {'title': 'H5:', 'name': 'h5info', 'type': 'group', 'children': [
             {'title': 'H5 Path:', 'name': 'h5path', 'type': 'str', 'value': '', 'readonly': True},
             {'title': 'Node:', 'name': 'node_path', 'type': 'list', 'limits': [], 'readonly': True},
-            {'title': 'Actuator:', 'name': 'actuator', 'type': 'str', 'value': '', 'readonly': True},
+            {'title': 'Actuator:', 'name': 'actuator', 'type': 'list', 'limits': [], 'readonly': True},
         ]},
         {'title': 'Histo:', 'name': 'histo', 'type': 'group', 'children': [
             {'title': 'Start:', 'name': 'start', 'type': 'float', 'value': 500.},
@@ -66,33 +66,56 @@ class HistogramPlot(CustomApp):
         self.worker.dte_signal.connect(self.viewer.show_data)
         self.worker.nbins_signal.connect(self.settings.child('histo', 'nbins').setValue)
 
-        self.runner_thread = QtCore.QThread()
-        self.worker.moveToThread(self.runner_thread)
-        self.runner_thread.start()
+        # self.runner_thread = QtCore.QThread()
+        # self.worker.moveToThread(self.runner_thread)
+        # self.runner_thread.start()
 
     def update_h5_saver(self, h5saver: H5Saver | str | Path,
                         node: str = 'RawData/Ramp000'):
         self._settings.sigTreeStateChanged.disconnect(self.parameter_tree_changed)
         if isinstance(h5saver, H5Saver):
             self.settings['h5info', 'h5path'] = str(h5saver.file_path)
-
+            self._h5saver = h5saver
         else:
             self.settings['h5info', 'h5path'] = str(h5saver)
-        self._h5saver = h5saver
+            self._h5saver = H5Saver()
+            self._h5saver.init_file(addhoc_file_path=h5saver)
+        self.update_node()
+        self.update_actuator()
+
+    def update_node(self, node_name: str | Node = None):
+        if isinstance(node_name, Node):
+            node_name = node_name.path
         nodes = []
-        with DataLoader(h5saver, swmr_mode=True) as dl:
+        with DataLoader(self.h5saver, swmr_mode=True) as dl:
             for ind, _node in enumerate(dl.walk_nodes('/RawData', depth=1, only_groups=True)):
                 if ind > 0:
                     nodes.append(_node.path)
+        try:
+            self._settings.sigTreeStateChanged.disconnect(self.parameter_tree_changed)
+        except TypeError:
+            pass
+        if node_name is None or node_name not in nodes:
+            node_name = nodes[0]
         with self.settings.child('h5info', 'node_path').treeChangeBlocker():
-            self.settings.child('h5info', 'node_path').setOpts(value=node, limits=nodes)
+            self.settings.child('h5info', 'node_path').setOpts(value=node_name, limits=nodes)
         self._settings.sigTreeStateChanged.connect(self.parameter_tree_changed)
 
-    def update_node(self, node_name: str | Node):
-        if isinstance(node_name, Node):
-            node_name = node_name.path
-        self._settings.sigTreeStateChanged.disconnect(self.parameter_tree_changed)
-        self.settings.child('h5info', 'node_path').setValue(node_name)
+    def update_actuator(self, actuator: str = None):
+        actuators = []
+        with DataLoader(self.h5saver, swmr_mode=True) as dl:
+            for ind, node in enumerate(dl.walk_nodes('/RawData', depth=2, only_groups=True)):
+                if ('type' in node.attrs and node.attrs['type'] == 'actuator' and
+                len(node.children()) > 0):
+                    actuators.append(node.title)
+        try:
+            self._settings.sigTreeStateChanged.disconnect(self.parameter_tree_changed)
+        except TypeError:
+            pass
+        if actuator is None or actuator not in actuators:
+            actuator = actuators[0]
+        with self.settings.child('h5info', 'actuator').treeChangeBlocker():
+            self.settings.child('h5info', 'actuator').setOpts(value=actuator, limits=actuators)
         self._settings.sigTreeStateChanged.connect(self.parameter_tree_changed)
 
     def value_changed(self, param: Parameter):
@@ -105,8 +128,13 @@ class HistogramPlot(CustomApp):
             self.compute_plot_histogram(self.settings['h5info', 'actuator'])
         elif param.name() == 'node_path':
             self.compute_plot_histogram(self.settings['h5info', 'actuator'])
+        elif param.name() == 'actuator':
+            self.compute_plot_histogram(param.value())
 
     def compute_plot_histogram(self, xaxis_name: str):
+        actuators = self.settings.child('h5info', 'actuator').opts['limits']
+        if xaxis_name not in actuators:
+            xaxis_name = actuators[0]
         self.settings['h5info', 'actuator'] = xaxis_name
 
         self.to_worker.emit(
@@ -131,7 +159,10 @@ class HistoWorker(QtCore.QObject):
         axis_name = histo_obj.axis_name
         dte_out = DataToExport('Histogram')
         logger.info('computing histogram')
-        with DataLoader(histo_obj.h5saver, swmr_mode=True) as dl:
+        file_path = histo_obj.h5saver
+        if isinstance(file_path, H5Saver):
+            file_path = file_path.file_path
+        with DataLoader(file_path, swmr_mode=True) as dl:
             dte = dl.load_all(where=histo_obj.node_path)
         if axis_name not in dte.get_names():
             self.dte_signal.emit(dte_out)
@@ -147,7 +178,8 @@ class HistoWorker(QtCore.QObject):
             xdwa_sliced = xdwa.inav[istart:istop]
         except IndexError:
             xdwa_sliced = xdwa
-        # first compute bins from the timestamps
+
+        # first compute bins from one of the varying signals
         timestamps = xdwa_sliced.get_axis_from_index(nav_index)[0].get_data()
 
         if histo_obj.nbins == 'auto':
@@ -164,46 +196,69 @@ class HistoWorker(QtCore.QObject):
         indexes = np.digitize(timestamps, bin_edges)
 
         # average actuator data in their corresponding bins:
-        averaged_actuator_values = np.atleast_1d(self.average_data_over_indexes(xdwa_sliced[0], indexes))
-
-
+        averaged_actuator_values = np.atleast_1d(
+            self.average_data_over_indexes(xdwa[0], indexes))
 
         for dwa in dte:
             indexes = np.digitize(dwa.get_axis_from_index(nav_index)[0].get_data(), bin_edges)
-            dte_out.append(DataCalculated(
-                dwa.name, origin=dwa.origin,
-                data = [np.atleast_1d(self.average_data_over_indexes(dwa[ind], indexes)) for ind in
-                        range(len(dwa))],
-                axes = [Axis(label = xdwa.name, units=xdwa.units,
-                             data=averaged_actuator_values)] +
-                       [dwa.get_axis_from_index(ind)[0] for ind in dwa.sig_indexes],
-                labels=dwa.labels,
-                units = dwa.units,
-            ))
+            arrays = [np.atleast_1d(
+                self.average_data_over_indexes(dwa[ind],
+                                               indexes,
+                                               len(averaged_actuator_values)-1)) for ind in
+                        range(len(dwa))]
+            try:
+                dwa_processed = DataCalculated(
+                    dwa.name, origin=dwa.origin,
+                    data = arrays,
+                    axes = [Axis(label=xdwa.name, units=xdwa.units,
+                                 data=averaged_actuator_values,
+                                 index=nav_index)] +
+                           [dwa.get_axis_from_index(ind)[0] for ind in dwa.sig_indexes],
+                    labels=dwa.labels,
+                    units = dwa.units,
+                    nav_indexes=(nav_index,) if len(dwa.sig_indexes) > 0 else ( ),
+                    distribution=DataDistribution.uniform,
+                )
+                dte_out.append(dwa_processed)
+            except IndexError as e:
+                pass
         self.dte_signal.emit(dte_out)
         return dte_out
 
     @staticmethod
     def average_data_over_indexes(data: np.ndarray[float],
-                                  indexes: np.ndarray[int]) -> np.ndarray[float]:
+                                  indexes: np.ndarray[int],
+                                  index_max: int = None) -> np.ndarray[float]:
         """ Return the average  of data over the indexes
 
         See: https://stackoverflow.com/questions/71329884/python-numpy-get-average-of-array-based-on-index
         """
-        one_hot = np.eye(np.max(indexes) + 1)[indexes]
+        try:
+            if index_max is None:
+                index_max = indexes.max()
 
-        counts = np.sum(one_hot, axis=0)
-        return np.sum((one_hot.T * data), axis=1) / counts
+            one_hot = np.eye(index_max + 1)[indexes]
+
+            counts = np.sum(one_hot, axis=0)
+            one_hot_t = one_hot.T
+            for ind in range(len(data.shape) - len(one_hot_t.shape) + 1):
+                one_hot_t = np.expand_dims(one_hot_t, axis=ind+2)
+                counts = np.expand_dims(counts, axis=ind+1)
+            res = np.sum((one_hot_t * data[0:len(indexes)]), axis=1) / counts
+        except ValueError as e:
+            res = np.linspace(0, indexes.max(), index_max + 1)
+        return res
 
 
 if __name__ == '__main__':
     app = mkQApp('Histogram')
 
-    file_path = r'C:\Data\2026\20260804\Dataset_20260804_008.h5'
+    file_path = r'C:\Data\2026\20260804\Dataset_20260804_040.h5'
 
     win, area = make_window(title='Histogram', flags=None)
     histo = HistogramPlot(dockarea=area)
     histo.settings.child('h5info', 'node_path').setOpts(readonly=False)
+    histo.settings.child('h5info', 'actuator').setOpts(readonly=False)
     histo.update_h5_saver(file_path)
 
     dock_settings = Dock('Settings')
